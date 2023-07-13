be41b061294d924a83195061c4c1d7cfcc76c21a
ed6978 <Spanian@hotmail.co.uk>
2023-07-13 19:13:13 +0100

    added reset function to clear grid

--- script.js
+++ script.js
@@ -7,9 +7,27 @@ function generateGrid(rows, cols) {
     for (let c = 0; c < cols; c++) {
       const gridCell = document.createElement("div");
       gridCell.classList.add("grid-cell");
+      gridCell.addEventListener("click", changeColor);
       gridContainer.appendChild(gridCell);
     }
   }
 }
 generateGrid(16, 16);
-console.log(generateGrid);
+
+function changeColor(event) {
+  event.target.style.backgroundColor = "black";
+}
+console.log(changeColor);
+
+function resetGridCell() {
+  const gridCells = document.querySelectorAll(".grid-cell");
+  gridCells.forEach((cell) => {
+    cell.style.backgroundColor = "";
+  });
+}
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+resetButton.classList.add("reset-button");
+resetButton.addEventListener("click", resetGridCell);
+document.body.appendChild(resetButton);
