767ae478215c732b4b06b861964d3a500b7a2a27
ed6978 <Spanian@hotmail.co.uk>
2023-07-24 16:29:33 +0100

    added a grid size function for user choice of format

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 const gridContainer = document.getElementById("grid-container");
 const gridButtonGenerator = document.createElement("button");
+const buttonContainer = document.createElement("div");
+const gridCell = document.createElement("div");
+
+//button container to style buttons//
+buttonContainer.classList.add("button-container");
 
 //creates and generates grids //
-function generateGrid(rows, cols) {
-  for (let r = 0; r < rows; r++) {
-    for (let c = 0; c < cols; c++) {
-      const gridCell = document.createElement("div");
-      gridCell.classList.add("grid-cell");
-      gridCell.addEventListener("mouseover", changeColor); // color squares
-      gridCell.addEventListener("click", eraser); // eraser
-      gridContainer.appendChild(gridCell);
-    }
+function generateGrid(size) {
+  gridContainer.innerHTML = "";
+  gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    const cell = gridCell.cloneNode();
+    cell.classList.add("grid-cell");
+    cell.addEventListener("mouseover", changeColor); // color squares
+    cell.addEventListener("mouseover", eraser); // eraser
+    gridContainer.appendChild(cell);
   }
 }
+
+// user choice on grid size through prompt//
+// function setGridSize() {
+//   let gridSizeInput = prompt("What size would you like the grid?");
+//   let gridSize = parseInt(gridSizeInput, 10);
+
+//   if (isNan(gridSize) || gridSize <= 0) {
+//     alert("Please enter a valid positive number for your chosen grid size");
+//     return;
+//   }
+//   generateGrid(gridSize);
+
+//   gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+//   gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+// }
+
 generateGrid(16, 16);
 
 //drawing color //
@@ -39,20 +62,11 @@ resetButton.classList.add("reset-button");
 resetButton.addEventListener("click", resetGridCell);
 document.body.appendChild(resetButton);
 
-// 34x34 square grid button//
-
-const userGridChoiceOne = document.createElement("button");
-userGridChoiceOne.textContent = "34x34";
-userGridChoiceOne.classList.add("user-choice-one");
-
-// userGridChoiceOne.addEventListener("click", function () {
-//   generateGrid(34, 34);
-// });
-// document.body.appendChild(userGridChoiceOne);
-
 // eraser //
 const eraserBtn = document.createElement("button");
 eraserBtn.textContent = "Eraser";
+eraserBtn.classList.add("eraser");
+
 eraserBtn.addEventListener("click", function () {
   gridContainer.querySelectorAll(".grid-cell").forEach((cell) => {
     cell.removeEventListener("mouseover", changeColor);
@@ -63,8 +77,9 @@ document.body.appendChild(eraserBtn);
 
 // color button to use after using an eraser
 const colorBtn = document.createElement("button");
-colorBtn.textContent = "Color";
+colorBtn.textContent = "Pen";
 colorBtn.classList.add("color-btn");
+
 colorBtn.addEventListener("click", function () {
   gridContainer.querySelectorAll(".grid-cell").forEach((cell) => {
     cell.removeEventListener("mouseover", eraser);
@@ -72,3 +87,31 @@ colorBtn.addEventListener("click", function () {
   });
 });
 document.body.appendChild(colorBtn);
+
+//user grid size input //
+
+// user grid size choice
+let setGridSizeBtn = document.createElement("button");
+setGridSizeBtn.textContent = "Grid Size";
+setGridSizeBtn.classList.add("user-grid-choice");
+setGridSizeBtn.addEventListener("click", function () {
+  const value = prompt("What size would you like the grid?");
+  if (typeof value === "undefined") {
+    return;
+  }
+  if (value > 100) {
+    value = alert("Sorry that value is not supported");
+  } else if (value <= 100) {
+    generateGrid(value);
+  }
+});
+
+document.body.appendChild(setGridSizeBtn);
+
+//buttons //
+buttonContainer.appendChild(colorBtn);
+buttonContainer.appendChild(eraserBtn);
+buttonContainer.appendChild(resetButton);
+buttonContainer.appendChild(setGridSizeBtn);
+
+document.body.appendChild(buttonContainer);
